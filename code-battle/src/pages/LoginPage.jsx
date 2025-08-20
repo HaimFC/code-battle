27ef75970ec2f8e6d8347b86df71b8fa3380a45f
jonahhess <jonahhessdev@gmail.com>
2025-08-20 14:57:48 +0300

    entered fields, mock button press

--- code-battle/src/pages/LoginPage.jsx
+++ code-battle/src/pages/LoginPage.jsx
@@ -1,1 +1,20 @@
+// Jonah
+
+// TODO: Use Mantine Form to pass values / UseState
+// TODO: Use supabase handleLogin function
+
 // Login form (uses Supabase Auth).
+
+function handleLoginMock(email, password) {
+  console.log("login button pressed");
+}
+
+export default function LoginPage() {
+  return (
+    <>
+      <input type="text" placeholder="enter email" />
+      <input type="text" placeholder="enter password" />
+      <button onClick={handleLoginMock}>Log In</button>
+    </>
+  );
+}
