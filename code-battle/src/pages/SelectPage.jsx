c2ab229d5ae85151c17a90c8caceb2c793bc3cfe
jonahhess <jonahhessdev@gmail.com>
2025-08-20 23:34:31 +0300

    mode prop

--- code-battle/src/pages/SelectPage.jsx
+++ code-battle/src/pages/SelectPage.jsx
@@ -1,13 +1,13 @@
 import Cards from "../components/Cards";
 
 // Shows list of coding questions.
-export default function SelectPage({ setDifficulty, handleStartCoding }) {
+export default function SelectPage({ mode, setDifficulty, handleStartCoding }) {
   const cards = [{ text: "Easy" }, { text: "Medium" }, { text: "Hard" }];
 
   return (
     <>
       <Cards cards={cards} handleClick={setDifficulty} />
-      <button onClick={handleStartCoding}>Practice</button>
+      <button onClick={handleStartCoding}>{mode}</button>
     </>
   );
 }
