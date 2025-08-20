fd8021347e331a5e86e10126007ac2aaea703881
jonahhess <jonahhessdev@gmail.com>
2025-08-20 21:35:08 +0300

    prop drill

--- code-battle/src/components/Cards.jsx
+++ code-battle/src/components/Cards.jsx
@@ -4,7 +4,9 @@ export default function Cards({ cards }) {
   return (
     <>
       {cards &&
-        cards?.map(({ text }, index) => <Card key={index} text={text} />)}
+        cards?.map(({ text, to }, index) => (
+          <Card key={index} text={text} to={to} />
+        ))}
     </>
   );
 }
