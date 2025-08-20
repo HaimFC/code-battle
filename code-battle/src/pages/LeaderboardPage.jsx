6d9efd00b7940fbd8621002f2a4bb7d2d3ea2858
jonahhess <jonahhessdev@gmail.com>
2025-08-20 19:48:25 +0300

    simple leaderboard implementation

--- code-battle/src/pages/LeaderboardPage.jsx
+++ code-battle/src/pages/LeaderboardPage.jsx
@@ -1,1 +1,41 @@
 // Shows leaderboard table.
+import { useEffect, useState } from "react";
+import { getMockLeaderboard } from "../utils/supabaseQueries";
+import LeaderboardEntry from "../components/LeaderboardEntry";
+
+export default function LeaderboardPage() {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const leaderboard = await getMockLeaderboard();
+      setLeaderboard(leaderboard);
+    }
+    getData();
+  }, []);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rating</th>
+            <th>Rank</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard &&
+            leaderboard.map(({ displayName, elo }, index) => (
+              <LeaderboardEntry
+                key={index}
+                displayName={displayName}
+                elo={elo}
+                rank={index}
+              />
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
