65ecc03257494ea15115355ac54727e5a60b6325
jonahhess <jonahhessdev@gmail.com>
2025-08-20 15:33:02 +0300

    navigation bar based on user and links

---
+++ code-battle/src/components/Navigation.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router";
+
+function mockUseAuth() {
+  return { activeUser: null };
+}
+
+const Navigation = ({ links }) => {
+  const { activeUser } = mockUseAuth();
+  const userOrGuest = activeUser ? "user" : "guest";
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: "1rem",
+        minHeight: "60px",
+        padding: "0 1rem",
+        boxSizing: "border-box",
+        backgroundColor: "lightgrey",
+      }}
+    >
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {links[userOrGuest]?.map(({ to, text }) => (
+          <Link key={to} to={to}>
+            {text}
+          </Link>
+        ))}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "1rem",
+          justifyContent: "flex-end",
+        }}
+      >
+        <p>Welcome {activeUser ?? "guest"}</p>
+      </div>
+    </div>
+  );
+};
+export default Navigation;
