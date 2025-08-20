55046b0a4b2a7e497e449aec6298ded96b7413f1
jonahhess <jonahhessdev@gmail.com>
2025-08-20 18:22:29 +0300

    moved mock auth into own file

--- code-battle/src/pages/LoginPage.jsx
+++ code-battle/src/pages/LoginPage.jsx
@@ -1,20 +1,19 @@
 // Jonah
 
+import { useMockAuth } from "../auth/AuthProvider";
+
 // TODO: Use Mantine Form to pass values / UseState
 // TODO: Use supabase handleLogin function
 
 // Login form (uses Supabase Auth).
 
-function handleLoginMock(email, password) {
-  console.log("login button pressed");
-}
-
 export default function LoginPage() {
+  const { handleLogin } = useMockAuth();
   return (
     <>
       <input type="text" placeholder="enter email" />
       <input type="text" placeholder="enter password" />
-      <button onClick={handleLoginMock}>Log In</button>
+      <button onClick={handleLogin}>Log In</button>
     </>
   );
 }
