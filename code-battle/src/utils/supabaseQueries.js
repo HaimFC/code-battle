c5592d8b39a6bbd2cc00ffc1a875dc64b1c7efae
jonahhess <jonahhessdev@gmail.com>
2025-08-21 00:50:43 +0300

    added post, get battle

--- code-battle/src/utils/supabaseQueries.js
+++ code-battle/src/utils/supabaseQueries.js
@@ -1,3 +1,5 @@
+import { useMockAuth } from "../auth/AuthProvider";
+
 export async function getMockLeaderboard() {
   const data = [
     { displayName: "john", elo: 1000 },
@@ -8,4 +10,32 @@ export async function getMockLeaderboard() {
   ];
 
   return data;
+}
+export async function getOpponent(difficulty) {
+  return { displayName: "alex", elo: 650 };
+}
+
+const QUESTIONS = { Easy: 13, Medium: 10, Hard: 25 };
+
+async function getMockRandomQuestionByDifficulty(difficulty) {
+  return "two sum";
+}
+
+export async function postMockBattle(opponent, difficulty) {
+  // const { activeUser } = useMockAuth();
+  // if (!(difficulty in QUESTIONS)) {
+  //   throw new Error("difficulty not found");
+  // }
+
+  // const question = await getMockRandomQuestionByDifficulty();
+  // const post = { userA: activeUser, userB: opponent, question };
+  return 3;
+}
+
+export async function getMockBattleByID(battleID) {
+  return {
+    userA: { displayName: "john" },
+    userB: { displayName: "alex" },
+    question: "two sum",
+  };
 }
