f19d9bcaf4685997e58bcf3e5a517aa9ab445321
jonahhess <jonahhessdev@gmail.com>
2025-08-21 08:29:26 +0300

    use activeUser

--- code-battle/src/pages/SelectPage.jsx
+++ code-battle/src/pages/SelectPage.jsx
@@ -1,13 +1,15 @@
+import { useMockAuth } from "../auth/AuthProvider";
 import Cards from "../components/Cards";
 
 // Shows list of coding questions.
 export default function SelectPage({ mode, setDifficulty, handleStartCoding }) {
+  const { activeUser } = useMockAuth();
   const cards = [{ text: "Easy" }, { text: "Medium" }, { text: "Hard" }];
 
   return (
     <>
       <Cards cards={cards} handleClick={setDifficulty} />
-      <button onClick={handleStartCoding}>{mode}</button>
+      <button onClick={() => handleStartCoding(activeUser)}>{mode}</button>
     </>
   );
 }
