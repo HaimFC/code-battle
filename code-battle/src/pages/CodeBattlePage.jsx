1b017f189cf27601f4bff68834be1ab6cec6acf9
jonahhess <jonahhessdev@gmail.com>
2025-08-21 08:29:43 +0300

    conditional text

--- code-battle/src/pages/CodeBattlePage.jsx
+++ code-battle/src/pages/CodeBattlePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getMockBattleByID } from "../utils/supabaseQueries";
+import { useMockAuth } from "../auth/AuthProvider";
 
 export default function CodeBattlePage({
   battleID,
@@ -7,19 +8,27 @@ export default function CodeBattlePage({
   handleForfeitBattle,
 }) {
   const [data, setData] = useState(null);
+  const { activeUser } = useMockAuth();
   useEffect(() => {
     async function getData() {
       const res = getMockBattleByID(battleID);
       setData(res);
     }
     getData();
+
+    return () => {
+      () => handleForfeitBattle(activeUser, battleID);
+    };
   }, []);
 
   return (
     <>
-      code battle
+      {battleID ? "Code Battle!" : "Practice Coding"}
+      {data && data?.question}
       <button onClick={handleFinishCoding}>Submit</button>
-      <button onClick={handleForfeitBattle}>Forfeit Battle</button>
+      <button onClick={handleForfeitBattle}>
+        {battleID ? "Forfeit Battle" : "Quit"}
+      </button>
     </>
   );
 }
