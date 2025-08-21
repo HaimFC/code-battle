b5df1d7431e2a98f8532e9dd03fbd8ddd48b60a0
ordamri <ordamry19@gmail.com>
2025-08-21 09:12:35 +0300

    Add judge0 api client (createSubmission

--- code-battle/src/api/judge0.js
+++ code-battle/src/api/judge0.js
@@ -1,3 +1,21 @@
 // Function for sending code to Judge0 API for execution.
 
-import.meta.env.VITE_JUDGE0_KEY
+export async function createSubmission(sourceCode, languageId, stdin = "") {
+  const url = `${import.meta.env.VITE_JUDGE0_BASE}/submissions?base64_encoded=false&wait=true`;
+
+  const headers = {
+    "Content-Type": "application/json",
+    "X-RapidAPI-Key": import.meta.env.VITE_JUDGE0_KEY,
+    "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com"
+  };
+
+  const body = JSON.stringify({
+    source_code: sourceCode,
+    language_id: languageId,
+    stdin
+  });
+
+  const res = await fetch(url, { method: "POST", headers, body });
+  if (!res.ok) throw new Error(`Judge0 error: HTTP ${res.status}`);
+  return res.json(); // => { stdout, stderr, status, time, memory, ... }
+}
