8580d5f141ff06005104ff536e7f90f8fc0dc760
jonahhess <jonahhessdev@gmail.com>
2025-08-21 08:29:08 +0300

    using custom hook

--- code-battle/src/pages/WaitingRoomPage.jsx
+++ code-battle/src/pages/WaitingRoomPage.jsx
@@ -1,23 +1,17 @@
-import { useEffect } from "react";
 import { getMockOpponent } from "../utils/supabaseQueries";
+import { useInterval } from "../hooks/useInterval";
 
 // waiting for opponent
 export default function WaitingRoomPage({ onOpponentFound, difficulty }) {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      async function getData() {
-        const opponent = await getMockOpponent(difficulty);
-        if (opponent && opponent.displayName) {
-          onOpponentFound(opponent);
-        }
+  useInterval(() => {
+    async function getData() {
+      const opponent = await getMockOpponent(difficulty);
+      if (opponent && opponent.displayName) {
+        onOpponentFound(opponent);
       }
-      getData();
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+    }
+    getData();
+  });
 
   return <>Waiting for Opponent</>;
 }
