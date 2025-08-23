a4a2d785cf3f750a757710ea039084fc4e59f435
jonahhess <jonahhessdev@gmail.com>
2025-08-23 23:11:52 +0300

    success navigates user to homepage

--- code-battle/src/pages/SignupPage.jsx
+++ code-battle/src/pages/SignupPage.jsx
@@ -1,10 +1,19 @@
 // pages/SignUpPage.jsx
 import { useAuthContext } from "../context/AuthContext";
-import { Button, TextInput, PasswordInput } from "@mantine/core";
+import {
+  Button,
+  TextInput,
+  PasswordInput,
+  Card,
+  Container,
+  Stack,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { useNavigate } from "react-router";
 
 export default function SignUpPage() {
   const { signUp } = useAuthContext();
+  const navigate = useNavigate();
 
   const form = useForm({
     mode: "uncontrolled",
@@ -21,64 +30,76 @@ export default function SignUpPage() {
       password: (v) => (v.length >= 6 ? null : "Password too short"),
       displayName: (v) => (v.trim().length >= 2 ? null : "Nickname too short"),
       firstName: (v) => (v ? null : null), // optional
-      lastName: (v) => (v ? null : null),  // optional
-      phone: (v) => (v && !/^\+?[0-9\s\-()]{6,}$/.test(v) ? "Invalid phone" : null),
+      lastName: (v) => (v ? null : null), // optional
+      phone: (v) =>
+        v && !/^\+?[0-9\s\-()]{6,}$/.test(v) ? "Invalid phone" : null,
     },
   });
 
   return (
-    <form
-      style={{ display: "flex", flexDirection: "column", gap: "0.5rem", maxWidth: 360 }}
-      onSubmit={form.onSubmit(async (values) => {
-        try {
-          await signUp({
-            email: values.email,
-            password: values.password,
-            displayName: values.displayName,
-            firstName: values.firstName,
-            lastName: values.lastName,
-            phone: values.phone,
-          });
-          alert("Account Created Successfully");
-        } catch (err) {
-          alert("Error " + (err?.message || "Unknown"));
-        }
-      })}
-    >
-      <TextInput
-        withAsterisk
-        placeholder="Email"
-        key={form.key("email")}
-        {...form.getInputProps("email")}
-      />
-      <PasswordInput
-        withAsterisk
-        placeholder="Password"
-        key={form.key("password")}
-        {...form.getInputProps("password")}
-      />
-      <TextInput
-        withAsterisk
-        placeholder="Nick name"
-        key={form.key("displayName")}
-        {...form.getInputProps("displayName")}
-      />
-      <TextInput
-        placeholder="First name"
-        key={form.key("firstName")}
-        {...form.getInputProps("firstName")}
-      />
-      <TextInput
-        placeholder="Last name"
-        key={form.key("lastName")}
-        {...form.getInputProps("lastName")}
-      />
-      <TextInput
-        placeholder="Phone"
-        key={form.key("phone")}
-        {...form.getInputProps("phone")}
-      />
-      <Button type="submit">Create Account</Button>
-    </form>
+    <Container size={"390px"}>
+      <Card radius={"10px"}>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.5rem",
+            maxWidth: 360,
+          }}
+          onSubmit={form.onSubmit(async (values) => {
+            try {
+              await signUp({
+                email: values.email,
+                password: values.password,
+                displayName: values.displayName,
+                firstName: values.firstName,
+                lastName: values.lastName,
+                phone: values.phone,
+              });
+              navigate("/login");
+            } catch (err) {
+              alert("Error " + (err?.message || "Unknown"));
+            }
+          })}
+        >
+          <Stack>
+            <TextInput
+              withAsterisk
+              placeholder="Email"
+              key={form.key("email")}
+              {...form.getInputProps("email")}
+            />
+            <PasswordInput
+              withAsterisk
+              placeholder="Password"
+              key={form.key("password")}
+              {...form.getInputProps("password")}
+            />
+            <TextInput
+              withAsterisk
+              placeholder="Nickname"
+              key={form.key("displayName")}
+              {...form.getInputProps("displayName")}
+            />
+            <TextInput
+              placeholder="First name"
+              key={form.key("firstName")}
+              {...form.getInputProps("firstName")}
+            />
+            <TextInput
+              placeholder="Last name"
+              key={form.key("lastName")}
+              {...form.getInputProps("lastName")}
+            />
+            <TextInput
+              placeholder="Phone"
+              key={form.key("phone")}
+              {...form.getInputProps("phone")}
+            />
+          </Stack>
+          <Button type="submit">Create Account</Button>
+        </form>
+      </Card>
+    </Container>
   );
 }
